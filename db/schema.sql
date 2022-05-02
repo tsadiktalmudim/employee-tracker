d755b6b5fae6c91f5fcda549777dda37665459fc
Xan <tsadiktalmudim@gmail.com>
2022-05-01 20:10:40 -0500

    fixed bugs

--- db/schema.sql
+++ db/schema.sql
@@ -1,16 +1,6 @@
-DROP DATABASE IF EXISTS employee-tracker-db;
-CREATE DATABASE employee-tracker-db;
-USE employee-tracker-db;
-
-CREATE TABLE employee (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INTEGER,
-    manager_id INTEGER
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
-    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
-);
+DROP DATABASE IF EXISTS employee_tracker_db;
+CREATE DATABASE employee_tracker_db;
+USE employee_tracker_db;
 
 CREATE TABLE department (
     id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -23,4 +13,14 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INTEGER,
     CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    manager_id INTEGER,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,16 +1,8 @@
-INSERT INTO employee (first_name, last_name, role_id, manager_id)
+INSERT INTO department (dept_name)
 VALUES
-    ("Devin", "Hickson", 1, null)
-    ("George", "Gino III", 5, null),
-    ("Lisa", "Kudrow", 3, null)
-    ("Minnie", "Hatfield", 5, 2),
-    ("Shayna", "Rourke", 2, 1),
-    ("Ben", "Stein", 2, 1),
-    ("Tiffany", "Sorenson", 4, 3),
-    ("Kumail", "Patel", 6, 2),
-    ("Monica", "Padman", 6, 2);
-
-
+    ("Sales"),
+    ("Accounting"),
+    ("Software Engineering");
 
 INSERT INTO role (title, salary, department_id)
 VALUES
@@ -21,8 +13,14 @@ VALUES
     ("Sales Manager", 130000, 1),
     ("Sales Agent", 35000, 1);
 
-INSERT INTO department (dept_name)
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES
-    ("Sales"),
-    ("Accounting"),
-    ("Software Engineering");
+    ("Devin", "Hickson", 1, null),
+    ("George", "Gino III", 5, null),
+    ("Lisa", "Kudrow", 3, null),
+    ("Minnie", "Hatfield", 5, 2),
+    ("Shayna", "Rourke", 2, 1),
+    ("Ben", "Stein", 2, 1),
+    ("Tiffany", "Sorenson", 4, 3),
+    ("Kumail", "Patel", 6, 2),
+    ("Monica", "Padman", 6, 2);
