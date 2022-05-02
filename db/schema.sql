3eef43e579c2311aed93ddfcdee74794ed9348fa
Xan <tsadiktalmudim@gmail.com>
2022-05-01 19:48:26 -0500

    added schema and starting data

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employee-tracker-db;
+CREATE DATABASE employee-tracker-db;
+USE employee-tracker-db;
+
+CREATE TABLE employee (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
+
+CREATE TABLE department (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INTEGER,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
---
+++ db/seeds.sql
@@ -0,0 +1,28 @@
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ("Devin", "Hickson", 1, null)
+    ("George", "Gino III", 5, null),
+    ("Lisa", "Kudrow", 3, null)
+    ("Minnie", "Hatfield", 5, 2),
+    ("Shayna", "Rourke", 2, 1),
+    ("Ben", "Stein", 2, 1),
+    ("Tiffany", "Sorenson", 4, 3),
+    ("Kumail", "Patel", 6, 2),
+    ("Monica", "Padman", 6, 2);
+
+
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ("Lead Software Engineer", 120000, 3),
+    ("Software Engineer", 85000, 3),
+    ("Accounting Manager", 120000, 2),
+    ("Accountant", 72000, 2),
+    ("Sales Manager", 130000, 1),
+    ("Sales Agent", 35000, 1);
+
+INSERT INTO department (dept_name)
+VALUES
+    ("Sales"),
+    ("Accounting"),
+    ("Software Engineering");
